9ac75da5dc1a2f0a62e69800b86be6bbb90d382a
Nikolas301998 <145227819+Nikolas301998@users.noreply.github.com>
2026-02-10 23:11:39 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,35 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 import { ValentineModal } from './components/ValentineModal';
-import { FloatingCart } from './components/FloatingCart';
 
 function App() {
-  const [cart, setCart] = useState<{ [key: string]: number }>({});
-
-  const addToCart = (id: string) => {
-    setCart(prev => ({ ...prev, [id]: (prev[id] || 0) + 1 }));
-  };
-
-  const removeFromCart = (id: string) => {
-    setCart(prev => {
-      const newCart = { ...prev };
-      if (newCart[id] > 1) newCart[id] -= 1;
-      else delete newCart[id];
-      return newCart;
-    });
-  };
-
   return (
     <div className="bg-black min-h-screen">
       <ValentineModal /> 
       <Navbar />
       <Hero />
-      <Menu cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} />
+      <Menu />
       <Footer /> 
-      <FloatingCart cart={cart} />
     </div>
   );
 }
